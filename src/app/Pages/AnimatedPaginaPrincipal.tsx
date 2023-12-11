42961af96d5db8ca8b8c6113c0b785a36571ddd6
Matheus Rocha <matheusrochaevs@gmail.com>
2023-12-11 17:59:02 -0300

    11/12 17h58

--- src/app/Pages/AnimatedPaginaPrincipal.tsx
+++ src/app/Pages/AnimatedPaginaPrincipal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import styled from "styled-components"
-import { Header } from '../../components/Header/index';
+import { Header } from '../../components/Header';
 
 interface IHeader {
     titulo1: React.ReactNode
